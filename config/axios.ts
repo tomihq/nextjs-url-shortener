8d7c7f8dcbb45bd9a69c2b5f8effea4dc6827245
Tomás Hernández <hernandeztomas584@gmail.com>
2023-12-28 19:28:38 -0300

    add: api key

--- config/axios.ts
+++ config/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
-import { API_URL } from "./constants";
+import { API_KEY, API_URL } from "./constants";
 
 export const apiInstance = axios.create({
     baseURL : API_URL,
+    headers: {
+      'x-api-key': API_KEY
+    }
   });
--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,5 @@
 export const NAME = process.env.NAME
 export const ICON = '/assets/icon.webp';
 export const ICON_FALLBACK = 'T'
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.API_URL;
+export const API_KEY = process.env.API_KEY
